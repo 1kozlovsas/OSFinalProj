c09382d0767e24d9faf2ece0c51bbb25ff39c9c7
Ian Kerr <lobotomization@gmail.com>
2016-12-07 09:29:07 -0500

    Got kernel threads working! Woo!

--- kernel_modules/module_2/myhook.c
+++ kernel_modules/module_2/myhook.c
@@ -6,12 +6,14 @@
 #include <linux/delay.h>
 #include <linux/kthread.h>
 
+struct task_struct *my_thread;
 
-/*int process_finder(void *input){
+int process_finder(void* input){
 	int highestpid; 
 	highestpid = 0;
 	char *process_name = "my_proc"; //Look for this process name
-	for(;;){
+	//for(;;){
+	while(!kthread_should_stop()){
 	struct task_struct *g;
 		for_each_process(g) { //Loop through all the processes
 			if(strcmp(process_name, g->comm) == 0){ //Find the processes which match the process_name
@@ -21,35 +23,39 @@
 				}
 			}
 	    	}
-	msleep(1000);	
-	}
-}*/
-
-
-static int __init hook_init(void)
-{
-	printk("Starting module\n");
-	//kthread_create(&process_finder,(void *)"Process finder","Process finder");
-	int highestpid; 
-	highestpid = 0;
-	char *process_name = "my_proc"; //Look for this process name
-	for(;;){
-	struct task_struct *g;
-		for_each_process(g) { //Loop through all the processes
-			if(strcmp(process_name, g->comm) == 0){ //Find the processes which match the process_name
-				if(g->pid > highestpid){
-					printk("%s has been started with pid %d\n", process_name, g->pid);
-					highestpid = g->pid;			
-				}
-			}
-	    	}
+	//schedule();
 	msleep(1000);	
 	}
 	return 0;
 }
 
+
+static int __init hook_init(void)
+{
+	printk("Starting module\n");
+	my_thread = kthread_run(&process_finder,NULL,"Process finder");
+	/*int highestpid;//, i; 
+	highestpid = 0;
+	char *process_name = "my_proc"; //Look for this process name
+	for(i = 0;i < 100; i++){ //100 second timeout
+	struct task_struct *g;
+		for_each_process(g) { //Loop through all the processes
+			if(strcmp(process_name, g->comm) == 0){ //Find the processes which match the process_name
+				if(g->pid > highestpid){
+					printk("%s has been started with pid %d\n", process_name, g->pid);
+					highestpid = g->pid;			
+				}
+			}
+	    	}
+	msleep(1000);	
+	}
+	*/
+	return 0;
+}
+
 static void __exit hook_cleanup(void)
 {
+	kthread_stop(my_thread);
 	printk("Closing module\n");
 }
 
